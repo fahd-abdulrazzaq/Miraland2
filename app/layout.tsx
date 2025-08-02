4fb3f2fa6abbbec2cb340687d28a2bfe9e418968
Fahd <fadabdul15@gmail.com>
2025-08-02 17:10:57 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'knights.app',
   description: 'knights',
-  generator: 'knghts',
+  generator: 'knights',
 }
 
 export default function RootLayout({
